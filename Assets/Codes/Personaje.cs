4e64e0d943937a531ba80e793b845df658b6fae4
pilielectrica <pilarmarco.musica@gmail.com>
2024-09-11 15:32:24 -0300

    agregados sonidos de pasos :footprints: :sound:

--- Assets/Codes/Personaje.cs
+++ Assets/Codes/Personaje.cs
@@ -7,13 +7,19 @@ public class Mover : MonoBehaviour
     [Header("Configuracion")]
     [SerializeField] float velocidad = 5f;
 
+    [Header("Audio")]
+    public AudioSource pasoAudioSource;
+    public AudioClip[] pasosClips; // Array de clips de pasos
+    public float tiempoEntrePasos = 0.5f; // Tiempo entre cada paso
+
     private float moverHorizontal;
     private float moverVertical;
     private Vector2 direccion;
 
     private Rigidbody2D miRigidbody2D;
     private Animator miAnimator;
-    public AudioSource llaveAudio;
+    private float temporizadorPasos;
+    private bool personajeMoviendose; // Para saber si el personaje se está moviendo
 
     private GameObject llaveColisionada;
     public GameObject VFXLlave;
@@ -22,6 +28,7 @@ public class Mover : MonoBehaviour
     {
         miRigidbody2D = GetComponent<Rigidbody2D>();
         miAnimator = GetComponent<Animator>();
+        temporizadorPasos = tiempoEntrePasos;
     }
 
     private void Update()
@@ -30,22 +37,47 @@ public class Mover : MonoBehaviour
         moverVertical = Input.GetAxis("Vertical");
         direccion = new Vector2(moverHorizontal, moverVertical);
 
-        // Actualizar los parámetros del Animator
+        // Comprobar si el personaje está en movimiento
         if (direccion.magnitude > 0.1f)
         {
+            personajeMoviendose = true;
             miAnimator.SetBool("IsMoving", true);
             miAnimator.SetFloat("moveX", moverHorizontal);
             miAnimator.SetFloat("moveY", moverVertical);
+
+            // Reproducir sonido de pasos solo si está en movimiento
+            temporizadorPasos -= Time.deltaTime;
+            if (temporizadorPasos <= 0)
+            {
+                ReproducirSonidoDePaso();
+                temporizadorPasos = tiempoEntrePasos; // Reiniciar temporizador
+            }
         }
         else
         {
+            // Si deja de moverse, parar los pasos
+            personajeMoviendose = false;
             miAnimator.SetBool("IsMoving", false);
+            temporizadorPasos = tiempoEntrePasos; // Reiniciar temporizador
         }
     }
 
     private void FixedUpdate()
     {
-        miRigidbody2D.MovePosition(miRigidbody2D.position + direccion * (velocidad * Time.fixedDeltaTime));
+        if (personajeMoviendose)
+        {
+            miRigidbody2D.MovePosition(miRigidbody2D.position + direccion * (velocidad * Time.fixedDeltaTime));
+        }
+    }
+
+    void ReproducirSonidoDePaso()
+    {
+        if (pasosClips.Length > 0 && personajeMoviendose)
+        {
+            int indice = Random.Range(0, pasosClips.Length);
+            AudioClip clipSeleccionado = pasosClips[indice];
+            pasoAudioSource.PlayOneShot(clipSeleccionado);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
