de5826d7f2015e0653e51990f974c2ff0c35e35d
pilielectrica <pilarmarco.musica@gmail.com>
2024-10-21 17:52:12 -0300

    SFX agarrar hada

--- Assets/Codes/Hada.cs
+++ Assets/Codes/Hada.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class Hada : MonoBehaviour
 {
+    public AudioSource HadaRecolectadaAudioSource;
+    public AudioClip hadaAudioClip;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Si el jugador colisiona con el hada
         if (other.CompareTag("Player"))
         {
             // Llama al método CollectFairy() del contador
             FindObjectOfType<ContadorDeHadas>().CollectFairy();
             
-            // Destruye el hada o realiza alguna acción
+            // Crear un objeto temporal solo para el sonido
+            GameObject audioObject = new GameObject("FairySound");
+            AudioSource audioSource = audioObject.AddComponent<AudioSource>();
+            audioSource.clip = hadaAudioClip;
+            audioSource.Play();
+            
+            // Destruir el objeto de sonido después de que termine la reproducción
+            Destroy(audioObject, hadaAudioClip.length);
+            
+            // Destruir el objeto hada inmediatamente
             Destroy(gameObject);
         }
     }
 }
+
